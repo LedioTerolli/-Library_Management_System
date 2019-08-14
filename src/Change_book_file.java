fc182fa69a0bb69ca8d913e70e84156bc51da51a
Ledio Terolli <ledio.terolli@gmail.com>
2019-08-13 23:39:24 -0400

    added 5000+ books

--- src/Change_book_file.java
+++ src/Change_book_file.java
@@ -1,7 +1,6 @@
-import com.sun.xml.internal.ws.api.ha.StickyFeature;
-
 import java.io.*;
-import java.util.Arrays;
+
+// used
 
 public class Change_book_file {
     public static void main(String[] args) {
@@ -21,13 +20,15 @@ public class Change_book_file {
             while ((line = reader.readLine()) != null) {
                 String sub_str = line.substring(1, line.length() - 1);
                 String[] book_parts = sub_str.split("\",\"");
+                if(book_parts.length != 7) continue;
+
                 nr_copies = (int) (Math.random() * 5) + 1;
 
                 for (int i = 1; i <= nr_copies; i++) {
                     StringBuilder str_b = new StringBuilder(); // outputting the serialized tree in this string
                     str_b.append("\"");
                     String[] book_parts_copy = new String[book_parts.length];
-                    for (int j = 0; j < book_parts.length; j++) book_parts_copy[j] = book_parts[j];
+                    book_parts_copy = book_parts.clone();
                     book_parts_copy[0] += Integer.toString(i);
 
                     for (int j = 0; j < interest_index.length; j++) {
