51a9d186ed4b0ece6aae6ace7a5984440899242a
Ledio Terolli <ledio.terolli@gmail.com>
2019-08-22 14:23:20 -0400

    100% DAO implementation

--- src/JLMS/dao/HoldDao.java
+++ src/JLMS/dao/HoldDao.java
@@ -6,9 +6,9 @@ import java.sql.Date;
 import java.util.List;
 
 public interface HoldDao extends GenericDao  {
-    Hold getByBookPatron(int book_id, String patron_username) throws Exception;
+    Hold getByBookPatron(long book_id, String patron_username) throws Exception;
 
     List<Hold> getByPatron_username(String patron_username) throws Exception;
 
-    List<Hold> getByBook_id(int book_id) throws Exception;
+    List<Hold> getByBook_id(long book_id) throws Exception;
 }
--- src/JLMS/daoimpl/HoldDaoImpl.java
+++ src/JLMS/daoimpl/HoldDaoImpl.java
@@ -12,7 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HoldDaoImpl implements HoldDao {
-    public HoldDaoImpl(){}
+    public HoldDaoImpl() {
+    }
 
     @Override
     public List getAll() throws Exception {
@@ -20,7 +21,7 @@ public class HoldDaoImpl implements HoldDao {
 
         List<Hold> holdList = new ArrayList<>();
         ResultSet rs;
-        Hold currentEmployee;
+        Hold currentHold;
 
         try (Connection conn = DBConn.getConnection();
              PreparedStatement statement = conn.prepareStatement("SELECT * FROM hold")
@@ -33,8 +34,8 @@ public class HoldDaoImpl implements HoldDao {
                 return holdList;
             } else {
                 while (rs.next()) {
-                    currentEmployee = extractEmployee(rs);
-                    holdList.add(currentEmployee);
+                    currentHold = extractHold(rs);
+                    holdList.add(currentHold);
                 }
             }
             conn.commit();
@@ -45,33 +46,151 @@ public class HoldDaoImpl implements HoldDao {
     }
 
     @Override
-    public Hold getByBookPatron(int book_id, String patron_username) throws Exception {
-        return null;
+    public Hold getByBookPatron(long book_id, String patron_username) throws Exception {
+        ResultSet rs;
+        Hold foundHold = null;
+
+        try (Connection conn = DBConn.getConnection();
+             PreparedStatement statement = conn.prepareStatement("SELECT * FROM HOLD WHERE book_id = ? AND patron_username = ?")
+        ) {
+            conn.setAutoCommit(false);
+            statement.setLong(1, book_id);
+            statement.setString(2, patron_username);
+            statement.execute();
+            rs = statement.getResultSet();
+            if (!rs.isBeforeFirst()) {
+                System.out.println("No results.");
+                return foundHold;
+            } else {
+                rs.next();
+                foundHold = extractHold(rs);
+            }
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return foundHold;
     }
 
     @Override
     public List<Hold> getByPatron_username(String patron_username) throws Exception {
-        return null;
+        ResultSet rs = null;
+        Hold currentHold = null;
+        List<Hold> holdList = new ArrayList<>();
+        try (
+                Connection conn = DBConn.getConnection();
+                PreparedStatement ps = conn.prepareStatement("SELECT * FROM HOLD WHERE patron_username = ?")
+        ) {
+            conn.setAutoCommit(false);
+            ps.setString(1, patron_username);
+            ps.execute();
+            rs = ps.getResultSet();
+
+            if (!rs.isBeforeFirst()) {
+                System.out.println("No results.");
+            } else {
+                while (rs.next()) {
+                    currentHold = extractHold(rs);
+                    holdList.add(currentHold);
+                }
+            }
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return holdList;
+
     }
 
     @Override
-    public List<Hold> getByBook_id(int book_id) throws Exception {
-        return null;
+    public List<Hold> getByBook_id(long book_id) throws Exception {
+        ResultSet rs = null;
+        Hold currentHold = null;
+        List<Hold> holdList = new ArrayList<>();
+        try (
+                Connection conn = DBConn.getConnection();
+                PreparedStatement ps = conn.prepareStatement("SELECT * FROM HOLD WHERE book_id = ?")
+        ) {
+            conn.setAutoCommit(false);
+            ps.setLong(1, book_id);
+            ps.execute();
+            rs = ps.getResultSet();
+
+            if (!rs.isBeforeFirst()) {
+                System.out.println("No results.");
+            } else {
+                while (rs.next()) {
+                    currentHold = extractHold(rs);
+                    holdList.add(currentHold);
+                }
+            }
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return holdList;
     }
+
+    //----------------------- ADD ---------------------------------------------------------------
 
     @Override
     public void add(Object objHold) throws Exception {
-
+        Hold hold = (Hold) objHold;
+        try (Connection conn = DBConn.getConnection();
+             PreparedStatement statement = conn.prepareStatement("INSERT INTO HOLD VALUES(?,?,?)")
+        ) {
+            conn.setAutoCommit(false);
+            buildStatement(hold, statement);
+            statement.executeUpdate();
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
+
+    //----------------------- UPDATE -------------------------------------------------------------
 
     @Override
     public void update(Object objHold) throws Exception {
+        Hold hold = (Hold) objHold;
 
+        try (Connection conn = DBConn.getConnection();
+             PreparedStatement statement = conn.prepareStatement(
+                     "UPDATE EMPLOYEE SET " +
+                             "BOOK_ID = ?, " +
+                             "PATRON_USERNAME = ?, " +
+                             "START_DATE = ?, " +
+                             "WHERE BOOK_ID = ? "
+             )
+        ) {
+            conn.setAutoCommit(false);
+            buildStatement(hold, statement);
+            statement.setLong(4, hold.getBook_id());
+            statement.executeUpdate();
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    //----------------------- DELETE -------------------------------------------------------------
 
     @Override
     public void delete(Object objHold) throws Exception {
-
+        Hold hold = (Hold) objHold;
+        try (Connection conn = DBConn.getConnection();
+             PreparedStatement statement = conn.prepareStatement("DELETE FROM HOLD WHERE book_id = ? and patron_username = ? ")
+        ) {
+            statement.setLong(1, hold.getBook_id());
+            statement.setString(2, hold.getPatron_username());
+            statement.executeUpdate();
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 
     //----------------------- HELPER METHODS -----------------------------------------------------
